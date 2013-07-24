c2bb5c1383c58efacd8566c0d81fbb9ed338e00f
allan <aerontwelve@yahoo.co.uk>
2013-07-24 23:29:09 +0000

    add more parameters in servlet

--- src/controller/MyFirstServlet.java
+++ src/controller/MyFirstServlet.java
@@ -20,7 +20,8 @@ public class MyFirstServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String address = request.getParameter("address");
+		
 	}
 
 }
